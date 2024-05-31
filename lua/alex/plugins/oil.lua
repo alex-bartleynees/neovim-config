1d1de16eac0377b1bb939b629a7ee31666b99ac7
Alexander Bartley Nees <alexbartleynees@gmail.com>
2024-05-31 23:36:29 +1200

    Set show hidden to true

--- lua/alex/plugins/oil.lua
+++ lua/alex/plugins/oil.lua
@@ -3,5 +3,12 @@ return {
 	opts = {},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
+	end,
 	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
 }
